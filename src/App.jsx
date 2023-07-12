3e61827ef767d5618dcb5cd05e8cafd9a8611640
Vika <svirin.victoria@gmail.com>
2023-07-12 11:46:23 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,93 @@
-import React, {useEffect, useState} from 'react';
-import { useProxy } from './useProxi';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useProxy } from "./useProxy";
+import "./App.css";
 import forecast from "./img/forecast.png";
-import forex_img from "./img/forex.png"
+import forex_img from "./img/forex.png";
 
 function App() {
-  const proxy = useProxy()
-   const [weather, setWeather] = useState([]);//weather info
-   const [forex, setForex] = useState([]);//forex info
-   const [cartoon, setCartoon] = useState([]);//cartoon img
+  const proxy = useProxy();
+  const [weather, setWeather] = useState([]); //weather info
+  const [forex, setForex] = useState([]); //forex info
+  const [cartoonList, setCartoonList] = useState([]); //cartoon collection
 
-  useEffect(()=>{
-    async function fetchData(){
-      const weatherData = await proxy.updateWeatherPr();//getting weather from the server
+  useEffect(() => {
+    async function fetchData() {
+      const weatherData = await proxy.updateWeatherPr(); //getting weather from the server
       setWeather(weatherData);
 
-      const forexData = await proxy.updateForexPr();//getting forex from the server
+      const forexData = await proxy.updateForexPr(); //getting forex from the server
       setForex(forexData);
 
-      const cartoonCollection = await proxy.updateCartoonPr();//getting cartoons collection from the server
-      
-      const cartoonItem = cartoonCollection&& cartoonCollection.map((singleCartoon)=>{ //getting single cartoon from the collection
-        return <div>
-          {singleCartoon.name}
-          {singleCartoon.image}</div>
-      });
-      setCartoon(cartoonItem);
-      console.log(cartoonItem);
-    }
-    fetchData();
-  }, []);
+      const cartoonCollection = await proxy.updateCartoonPr(); //getting cartoons collection from the server
+      // cartoonCollection = array of ojects
 
- 
-  
+      const cartoonElements = cartoonCollection.map((item) => {
+        return (
+          <div key={item.id} className="cartoon-item">
+            <div className="cartoon-header">{item.name}</div>
+            <div className="cartoon-image">
+              <img className="image-cartoon" src={item.image} alt=""/></div>
+          </div>
+        );
+      });
+      //cartoonElements = array of elements
+
+      setCartoonList(cartoonElements);
+      // cartoonList = array of elements
+    }
+
+    fetchData();
+
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Vikiport
-        </p>
+        <div>Vikiport</div>
       </header>
+
       <div className="divided-display">
-        <div className="cartoons-box">
-          <div className="cartoon-item">
-            {cartoon}
-          </div>
-          {/* <div className="cartoon-item">
-            {cartoon}
-          </div> */}
-          
-        </div>
+        <div className="cartoons-box">{cartoonList}</div>
+
         <div className="info-box">
           <div className="info info-item info-text-weather">
-            <div> <img src={forecast} className='bcg'/></div>
+            <div>
+              {" "}
+              <img src={forecast} className="bcg" alt="" />
+            </div>
             {weather}
           </div>
           <div className="info info-item info-text-forex">
-            <div> <img src={forex_img} className='bcg'/></div>
+            <div>
+              {" "}
+              <img src={forex_img} className="bcg" alt=""/>
+            </div>
             {forex}
           </div>
         </div>
       </div>
       <footer>
+          <p>
+          &copy; All rights reserved to Victoria Svirin 2023
+          </p>
         <p>
-        <a href="https://www.flaticon.com/free-icons/forex" title="forex icons">Forex icons created by juicy_fish - Flaticon</a>
-        <a href="https://www.flaticon.com/free-icons/foggy" title="foggy icons">Foggy icons created by Freepik - Flaticon</a><a href="https://www.flaticon.com/free-icons/humidity" title="humidity icons">Humidity icons created by Freepik - Flaticon</a>
-        <a href="https://www.flaticon.com/free-icons/forecast" title="forecast icons">Forecast icons created by ChilliColor - Flaticon</a>
-          <a href="https://www.flaticon.com/free-icons/temperature" title="temperature icons">Temperature icons created by Freepik - Flaticon</a></p>
+          <a
+            href="https://www.flaticon.com/free-icons/forex"
+            title="forex icons"
+          >
+            Forex icons created by juicy_fish - Flaticon
+          </a>
+          </p>
+          <p>
+          <a
+            href="https://www.flaticon.com/free-icons/forecast"
+            title="forecast icons"
+          >
+            Forecast icons created by ChilliColor - Flaticon
+          </a>
+          </p>
+          
       </footer>
     </div>
   );
